f5ddc901a29c68f164f6f1d1b75c2b35a63757d1
Abhinav Pradeep <32289221+abhinav2301@users.noreply.github.com>
2020-10-02 23:03:00 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/gkr3tf3fsl4ui2v/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/file/d/1xw5twmDsI6ct2I92eNfHrgNMdoXwaw6G/view?usp=sharing'
 export_file_name = 'export.pkl'
 
-classes = ['cat', 'dog']
+classes = ['Abyssinian', 'Bengal', 'Birman', 'Bombay cat', 'British_Shorthair', 'Budgerigars', 'Chinchilla', 'Cockatiel', 'Cockatoo', 'Conures', 'Dalmation', 'Dove', 'Egyptian_Mau', 'Gerbil', 'German Shepherd', 'Golden Retriever', 'Guinea Pig', 'Hamster', 'Husky dog', 'Indian Pariah', 'Labrador', 'Macaw', 'Maine_Coon', 'Mastiff dog', 'Mouse', 'Parakeet', 'Parrot', 'Persian', 'Pigeon', 'Poicephalus', 'Ragdoll', 'Rat', 'Russian_Blue', 'Siamese', 'Sphynx', 'Tabby cat', 'american_bulldog', 'american_pit_bull_terrier', 'basset_hound', 'beagle', 'boxer', 'chihuahua', 'english_cocker_spaniel', 'english_setter', 'german_shorthaired', 'great_pyrenees', 'havanese', 'japanese_chin', 'keeshond', 'leonberger', 'miniature_pinscher', 'newfoundland', 'pomeranian', 'pug', 'saint_bernard', 'samoyed', 'scottish_terrier', 'shiba_inu', 'staffordshire_bull_terrier', 'wheaten_terrier', 'yorkshire_terrier']
 path = Path(__file__).parent
 
 app = Starlette()
